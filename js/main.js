19118f47a5a5083868f2c5804a673cb180a60660
MrKrasnov <mr.krasnov.y@gmail.com>
2022-04-18 13:57:20 +0300

    1.2 created random show quotes

---
+++ js/main.js
@@ -0,0 +1,54 @@
+import { quotes } from "./quotes.js";
+
+function toggleLight() {
+    let toggle = document.getElementById('toggle');
+
+    toggle.onclick = (e) => {
+
+        if (e.target.classList.contains('toggle_night')) {
+            document.body.style.background = "#484343";
+            e.target.classList.remove('toggle_night');
+            e.target.classList.add('toggle_sun');
+        } else {
+            document.body.style.background = "#ffffff";
+            e.target.classList.remove('toggle_sun');
+            e.target.classList.add('toggle_night');
+        }
+
+    }
+}
+toggleLight()
+
+function quoteWritter() {
+    return new Promise(resolve => {
+        document.querySelector('.preloader').style.display = "none";
+        let num = Math.floor(Math.random() * (quotes.length - 0) + 0);
+        resolve([quotes[num][0], quotes[num][1]]);
+    })
+        .then(quote => {
+            if (document.querySelector('p') || document.querySelector('small')) {
+                document.querySelector('p').remove();
+                document.querySelector('small').remove();
+            }
+            let p = document.createElement('p');
+            p.textContent = quote[0];
+            let small = document.createElement('small');
+            small.textContent = quote[1];
+            document.querySelector('.scene').append(p);
+            document.querySelector('.scene').append(small);
+        });
+}
+
+
+
+document.querySelector('.btn').onclick = () => {
+    document.querySelector('.btn').remove();
+    document.querySelector('.preloader').style.display = "block";
+    setInterval(quoteWritter, 2000);
+};
+
+
+
+
+
+
---
+++ js/quotes.js
@@ -0,0 +1,35 @@
+export let quotes = [
+    [
+        'If one does not know to which port one is sailing, no wind is favorable.',
+        'Lucius Annaeus Seneca'
+    ],
+    [
+        'True happiness is... to enjoy the present, without anxious dependence upon the future.',
+        'Lucius Annaeus Seneca'
+    ],
+    [
+        'One of the most beautiful qualities of true friendship is to understand and to be understood.',
+        'Lucius Annaeus Seneca'
+    ],
+    [
+        'Sometimes even to live is an act of courage.',
+        'Lucius Annaeus Seneca'
+    ],
+    [
+        'All men are by nature equal, made all of the same earth by one workman.',
+        'Plato'
+    ],
+    [
+        'The measure of a man is what he does with power.',
+        'Plato'
+    ],
+    [
+        'The unexamined life is not worth living.',
+        'Socrates'
+    ],
+    [
+        'Strong minds discuss ideas, average minds discuss events, weak minds discuss people.',
+        'Socrates'
+    ],
+];
+
